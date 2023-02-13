7012404e4a25c719fd5ef4ca93ee9225b5023a2f
Santiago Tena Hernandez <santiago@Santiagos-MacBook-Air.local>
2023-02-13 22:21:31 +0100

    Solve exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	let splitString;
+	let revSplitString;
+	let length;
+	
+	splitString = string.split("");
+	revSplitString = [];
+	length = splitString.length
+	for (let i = 0; i < length; i++) {
+		revSplitString[(length - 1) - i] = splitString[i]; 
+	}
+	return (revSplitString.join(''));
 };
 
 // Do not edit below this line
