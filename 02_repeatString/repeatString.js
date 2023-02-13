4cc111bf657f485f9c855248209f764b39e70d4d
Santiago Tena Hernandez <santiago@Santiagos-MacBook-Air.local>
2023-02-13 19:13:53 +0100

    Solve first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+	let nString = "";
+	
+	if (n < 0)
+		return "ERROR"
+	if (n == 0)
+		return "";
+	for (let i = 0; i < n; i++) {
+		nString = nString + string;
+	}
+	return nString;
 };
 
 // Do not edit below this line
