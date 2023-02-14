116d412e422e942f9a99a8a28ce741ddcde531bd
Santiago Tena Hernandez <santiago@Santiagos-MacBook-Air.local>
2023-02-14 01:10:25 +0100

    Solve exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+	let argsSize;
+	let arraySize;
 
+	arraySize = array.length;
+	argsSize = args.length;
+	for (let i = 0; i < argsSize; i++) {
+		for (let j = 0; j < arraySize; j++) {
+			if (array[j] === args[i]) {
+				array.splice(j, 1);
+			}
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
