bb7ba395ae14050ccf297e0b818537afa80bdc5a
Santiago Tena Hernandez <santiago@Santiagos-MacBook-Air.local>
2023-02-14 18:48:15 +0100

    Solve exercises

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversion = (fahrenheit - 32) * (5 / 9);
+
+  return (Math.round(conversion * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = celsius * (9 / 5) + 32;
+
+  return (Math.round(conversion * 10) / 10);
 };
 
 // Do not edit below this line
