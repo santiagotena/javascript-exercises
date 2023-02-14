334cf6ad14fc86674120fabc54bfb1284aca8296
Santiago Tena Hernandez <santiago@Santiagos-MacBook-Air.local>
2023-02-14 17:28:47 +0100

    Solve exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+	let sumTotal = 0;
+
+	if (a < 0 || b < 0)
+		return ("ERROR");
+	if (typeof(a) != "number" || typeof(b) != "number")
+		return ("ERROR");
+	if (a < b) {
+		for (let i = a; i <= b; i++) {
+			sumTotal += i;
+		}
+	} else if (a > b) {
+		for (let i = b; i <= a; i++) {
+			sumTotal += i;
+		}
+	} else {
+		return (a);
+	}
+	
+	return (sumTotal);
 };
 
 // Do not edit below this line
