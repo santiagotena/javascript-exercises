610b886f5eac436edaf07d6f092df6806f2983af
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-07 10:43:23 +0100

    Solve exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+	let output;
 
+	output = [];
+	for (let i = 0; i < array.length; i++) {
+		output.push(array[i].title);
+	}
+	return (output);
 };
+
+const books = [
+{
+	title: 'Book',
+	author: 'Name'
+},
+{
+	title: 'Book2',
+	author: 'Name2'
+}
+]
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
