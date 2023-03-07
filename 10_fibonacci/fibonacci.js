adac2265cbd521ad7e7b92137b89c77b6277297f
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-07 10:32:49 +0100

    Solve exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+	let array;
+	let temp;
 
+	if (a < 1)
+		return ("OOPS");
+
+	array = [1, 1];
+	if (a === 1 || a === 2)
+		return (1);
+
+	for (let i = 2; i < a; i++) {
+		temp = array[i - 1] + array[i - 2];
+		array.push(temp);
+	}
+	
+	return (array[a - 1]);
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+// // Debugging
+// http://localhost:5500/top/forked_repos/javascript-exercises/10_fibonacci/
+// fibonacci(4);
