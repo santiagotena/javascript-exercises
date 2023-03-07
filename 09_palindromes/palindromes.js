2869731e9263abe0eec17edf8c5ea29de81170d4
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-07 09:36:43 +0100

    Solve exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	let length;
+	let front;
+	let reverse;
 
+	string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	string = string.replace(/\s/g, '');
+	string = string.toLowerCase();
+	length = string.length;
+	front = "";
+	reverse = "";
+	for (let i = 0; i <= Math.floor(length/2); i++)
+	{
+		front = front.concat("", string[i]);
+		reverse = reverse.concat("", string[(length - 1) - i]);
+	}
+	return (front === reverse);
 };
 
 // Do not edit below this line
