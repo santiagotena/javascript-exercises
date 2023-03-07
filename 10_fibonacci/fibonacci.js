aff54ee0b08ffe07f2eb4d6c062aebeb8b2d2d03
Santiago Tena Hernandez <stenahe@gmail.com>
2023-03-07 10:33:34 +0100

    Improve format

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,16 +4,13 @@ const fibonacci = function(a) {
 
 	if (a < 1)
 		return ("OOPS");
-
 	array = [1, 1];
 	if (a === 1 || a === 2)
 		return (1);
-
 	for (let i = 2; i < a; i++) {
 		temp = array[i - 1] + array[i - 2];
 		array.push(temp);
 	}
-	
 	return (array[a - 1]);
 };
 
